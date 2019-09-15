156020faf9a3b4a38714a12890cf5c02d1fa02bc
Sachin Singh <ssingh259@sapient.com>
2019-09-15 18:01:56 +0530

    Implemented port restrictions

--- dist/js/argonLogger.js
+++ dist/js/argonLogger.js
@@ -2,7 +2,7 @@
  * A simple console logging utility
  * Released under MIT license
  * @author Sachin Singh <contactsachinsingh@gmail.com>
- * @version v0.0.6
+ * @version v0.0.7
  * @license MIT
  */
 (function (global, factory) {
