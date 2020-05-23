dd56f90a6af97ade1446473f1266d4f3e8bebee2
Sachin Singh <ssingh259@sapient.com>
2020-05-23 17:52:20 +0530

    Bumped version

--- dist/js/argonLogger.js
+++ dist/js/argonLogger.js
@@ -2,7 +2,7 @@
  * A simple console logging utility
  * Released under MIT license
  * @author Sachin Singh <contactsachinsingh@gmail.com>
- * @version v0.2.0
+ * @version v0.2.1
  * @license MIT
  */
 (function (global, factory) {
